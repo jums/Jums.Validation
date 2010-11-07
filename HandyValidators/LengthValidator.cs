b83485110844bfc08212ca84111dabecdcad6f31
Juhani Markkula <juhani@logic.fi>
2010-11-08 00:17:40 +0200

    Added initial HandyValidators

---
+++ HandyValidators/LengthValidator.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Jums.Validation.HandyValidators
+{
+    /// <summary>
+    /// Validates that the target text input controls text length is within  given limits.
+    /// </summary>
+    /// <remarks>
+    /// Unlike standard Web Forms validators, this has dynamic display, focus on error and "error" css class by default.
+    /// </remarks>
+    [ToolboxData("<{0}:LengthValidator runat=server></{0}:LengthValidator>")]
+    public class LengthValidator : BaseValidator
+    {
+        /// <summary>
+        /// Minimum value string length. 0 = no limit.
+        /// </summary>
+        [DefaultValue(0)]
+        [Category("Handy")]
+        public int Min {
+            get { return (int)(this.ViewState["Min"] ?? 0); }
+            set { this.ViewState["Min"] = value; }
+        }
+        
+        /// <summary>
+        /// Maximum vlaue string length. 0 = no limit.
+        /// </summary>
+        [DefaultValue(0)]
+        [Category("Handy")]
+        public int Max
+        {
+            get { return (int)(this.ViewState["Max"] ?? 0); }
+            set { this.ViewState["Max"] = value; }
+        }
+
+        public LengthValidator() : base()
+        {
+            this.CssClass = "error";
+            this.Display = ValidatorDisplay.Dynamic;
+            this.SetFocusOnError = true;
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+
+            if (this.EnableClientScript && this.DetermineRenderUplevel())
+            {
+                string javascriptValidationFunction = CreateJavaScript();
+                Page.ClientScript.RegisterExpandoAttribute(this.ClientID, "evaluationfunction", "HandyCheckLength");
+                Page.ClientScript.RegisterExpandoAttribute(this.ClientID, "minLength", this.Min.ToString());
+                Page.ClientScript.RegisterExpandoAttribute(this.ClientID, "maxLength", this.Max.ToString());
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "JSScript", javascriptValidationFunction);
+            }
+        }
+
+        protected string CreateJavaScript()
+        {
+            return @"
+<script type=""text/javascript"">
+    function HandyCheckLength(validator){
+        var value = ValidatorTrim(ValidatorGetValue(validator.controltovalidate));
+        if (value.length == 0) return true;
+        if (validator.minLength > 0 && value.length < validator.minLength) return false;
+        if (validator.maxLength > 0 && value.length > validator.maxLength) return false;
+        return true;
+    }
+</script>";
+        }
+
+        protected override bool ControlPropertiesValid()
+        {
+            IEditableTextControl ctrl = FindControl(ControlToValidate) as IEditableTextControl;
+            return (ctrl != null);
+        }
+
+        protected override bool EvaluateIsValid()
+        {
+            return this.CheckInputLength();
+        }
+
+        protected bool CheckInputLength()
+        {
+            IEditableTextControl ctrl = ((IEditableTextControl)this.FindControl(this.ControlToValidate));
+            string value = (ctrl.Text ?? "").Trim();
+
+            if (this.Min > 0)
+            {
+                if (value.Length < this.Min)
+                {
+                    return false;
+                }
+            }
+
+            if (this.Max > 0)
+            {
+                if (value.Length > this.Max)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ HandyValidators/RequireCheckedValidator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Jums.Validation.HandyValidators
+{
+    /// <summary>
+    /// Validates that the target CheckBox control is checked.
+    /// </summary>
+    /// <remarks>
+    /// Unlike standard Web Forms validators, this has dynamic display, focus on error and "error" css class by default.
+    /// </remarks>
+    [ToolboxData("<{0}:RequireCheckedValidator runat=server></{0}:RequireCheckedValidator>")]
+    public class RequireCheckedValidator : BaseValidator
+    {
+        public RequireCheckedValidator() : base()
+        {
+            this.CssClass = "error";
+            this.Display = ValidatorDisplay.Dynamic;
+            this.SetFocusOnError = true;
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+
+            if (this.EnableClientScript && this.DetermineRenderUplevel())
+            {
+                string javascriptValidationFunction = CreateJavaScript();
+                Page.ClientScript.RegisterExpandoAttribute(this.ClientID, "evaluationfunction", "HandyCheckIfChecked");
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "JSScript", javascriptValidationFunction);
+            }
+            
+        }
+
+        protected string CreateJavaScript()
+        {
+            return @"
+<script type=""text/javascript"">
+    function HandyCheckIfChecked(validator){
+        var checkbox = document.getElementById(validator.controltovalidate);
+        return checkbox.checked;
+    }
+</script>";
+        }
+
+        protected override bool ControlPropertiesValid()
+        {
+            Control ctrl = this.FindControl(ControlToValidate) as CheckBox;
+            return (ctrl != null);
+        }
+
+        protected override bool EvaluateIsValid()
+        {
+            return this.CheckIfItemIsChecked();
+        }
+
+        protected bool CheckIfItemIsChecked()
+        {
+            CheckBox checkbox = ((CheckBox) this.FindControl(this.ControlToValidate));
+            return checkbox.Checked;
+        }
+    }
+}
